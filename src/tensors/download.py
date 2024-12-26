d7a3471f0534883a213ec4fca8a0990b0a4c6a89
DuyguA <duygu.altinok12@gmail.com>
2024-12-26 16:08:00 +0100

    added datasetwise download

--- src/tensors/download.py
+++ src/tensors/download.py
@@ -8,3 +8,10 @@ datasets = ["irony", "offense", "sst2", "sst5", "stance", "yelp5"]
 for dataset in datasets:
   fname = f"embeddings-{teacher_model}-{dataset}.jsonl"
   hf_hub_download(repo_id="BayanDuygu/LlamaTensors", filename=fname)
+
+
+for x in ["3", "8"]:
+  dataset="sst5"
+  fname = f"embeddings-llama{x}B-{dataset}.jsonl"
+  hf_hub_download(repo_id="BayanDuygu/LlamaTensors", filename="data/train/" + fname, repo_type="dataset", local_dir=".")
+
